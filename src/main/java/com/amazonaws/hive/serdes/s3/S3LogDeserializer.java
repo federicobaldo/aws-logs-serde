ca1d1518725836c94f1e21b4b851e0d73a5a6b8c
unknown <fbaldo@DUB-6900091951.ant.amazon.com>
2014-06-16 10:46:57 +0100

    Initial commit

---
+++ src/main/java/com/amazonaws/hive/serdes/s3/S3LogDeserializer.java
@@ -0,0 +1,142 @@
+package com.amazonaws.hive.serdes.s3;
+
+/**
+ * Created by fbaldo on 30/05/14.
+ */
+import java.nio.charset.CharacterCodingException;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.serde2.AbstractDeserializer;
+import org.apache.hadoop.hive.serde2.SerDeException;
+import org.apache.hadoop.hive.serde2.SerDeStats;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+
+/**
+ * S3LogDeserializer.
+ *
+ */
+public class S3LogDeserializer extends AbstractDeserializer {
+
+    public static final Log LOG = LogFactory.getLog(S3LogDeserializer.class
+            .getName());
+
+    static {
+        StackTraceElement[] sTrace = new Exception().getStackTrace();
+        sTrace[0].getClassName();
+    }
+
+    private ObjectInspector cachedObjectInspector;
+
+    @Override
+    public String toString() {
+        return "S3LogDeserializer[]";
+    }
+
+    public S3LogDeserializer() throws SerDeException {
+    }
+
+    static Pattern regexpat = Pattern
+            .compile("(\\S+) (\\S+) \\[(.*?)\\] (\\S+) (\\S+) (\\S+) (\\S+) (\\S+) (\".+\"|-) (\\S+) (\\S+) (\\S+) (\\S+) (\\S+) (\\S+) (\".+\"|-) (\".*\"|-) (.*)");
+
+    S3LogStruct deserializeCache = new S3LogStruct();
+
+    @Override
+    public void initialize(Configuration job, Properties tbl)
+            throws SerDeException {
+
+        cachedObjectInspector = ObjectInspectorFactory
+                .getReflectionObjectInspector(S3LogStruct.class,
+                        ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+
+        LOG.debug(getClass().getName() + ": initialized");
+    }
+
+    public static Integer toInt(String s) {
+        s = s.replace("\"","");
+        if (s.compareTo("-") == 0) {
+            return null;
+        } else {
+            return Integer.valueOf(s);
+        }
+    }
+    // Number of rows not matching the regex
+    long unmatchedRows = 0;
+
+    public Object deserialize(S3LogStruct c, String row) throws Exception {
+        Matcher match = regexpat.matcher(row);
+        int t = 1;
+        try {
+            match.matches();
+            c.bucketowner = match.group(t++);
+            c.bucketname = match.group(t++);
+            c.rdatetime = match.group(t++);
+            c.rip = match.group(t++);
+            c.requester = match.group(t++);
+            c.requestid = match.group(t++);
+            c.operation = match.group(t++);
+            c.rkey = match.group(t++);
+            c.requesturi = match.group(t++).replace("\"","");
+            c.httpstatus = toInt(match.group(t++));
+            c.errorcode = match.group(t++);
+            c.bytessent = toInt(match.group(t++));
+            c.objsize = toInt(match.group(t++));
+            c.totaltime = toInt(match.group(t++));
+            c.turnaroundtime = toInt(match.group(t++));
+            c.referer = match.group(t++).replace("\"","");
+            c.useragent = match.group(t++).replace("\"","");
+            c.versionid =  match.group(t++);
+        } catch (Exception e) {
+            LOG.error(new StringBuilder().append("Unmatched row found: ").append(row).append(" | ").append(e.toString()).toString());
+            unmatchedRows++;
+            if (unmatchedRows > 50) {
+                throw new SerDeException("S3 Log Regex did not match:" + row, e);
+            }
+        }
+        return (c);
+    }
+
+    @Override
+    public Object deserialize(Writable field) throws SerDeException {
+        String row = null;
+        if (field instanceof BytesWritable) {
+            BytesWritable b = (BytesWritable) field;
+            try {
+                row = Text.decode(b.getBytes(), 0, b.getLength());
+            } catch (CharacterCodingException e) {
+                throw new SerDeException(e);
+            }
+        } else if (field instanceof Text) {
+            row = field.toString();
+        }
+        try {
+            deserialize(deserializeCache, row);
+            return deserializeCache;
+        } catch (ClassCastException e) {
+            throw new SerDeException(this.getClass().getName()
+                    + " expects Text or BytesWritable", e);
+        } catch (Exception e) {
+            throw new SerDeException(e);
+        }
+    }
+
+    @Override
+    public ObjectInspector getObjectInspector() throws SerDeException {
+        return cachedObjectInspector;
+    }
+
+    @Override
+    public SerDeStats getSerDeStats() {
+        // no support for statistics
+        return null;
+    }
+}
---
+++ src/main/java/com/amazonaws/hive/serdes/s3/S3LogStruct.java
@@ -0,0 +1,31 @@
+package com.amazonaws.hive.serdes.s3;
+
+/**
+ * Created by fbaldo on 30/05/14.
+ */
+/**
+ * S3LogStruct.
+ *
+ */
+public class S3LogStruct {
+
+    public String bucketowner;
+    public String bucketname;
+    public String rdatetime;
+    public String rip;
+    public String requester;
+    public String requestid;
+    public String operation;
+    public String rkey;
+    public String requesturi;
+    public Integer httpstatus;
+    public String errorcode;
+    public Integer bytessent;
+    public Integer objsize;
+    public Integer totaltime;
+    public Integer turnaroundtime;
+    public String referer;
+    public String useragent;
+    public String versionid;
+
+}
